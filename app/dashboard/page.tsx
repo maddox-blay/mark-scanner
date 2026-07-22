6d5e781524f964045d52941d558b8da22d7f8c9d
maddox-blay <156577870+maddox-blay@users.noreply.github.com>
2026-07-22 19:12:16 +0000

    responsive padding on dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { ClassCard } from "@/components/ui/class-card";
 
 export default async function Page() {
     return (
-        <div className="p-12">
+        <div className=" px-4 md: p-12">
             <h1 className="text-4xl">Your classes</h1>
             <div className="py-8"><ClassCard /></div>
         </div>
