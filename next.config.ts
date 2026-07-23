ffdfb8aada54f89094ed902d4b8ba74eaaafedbd
maddox-blay <156577870+maddox-blay@users.noreply.github.com>
2026-07-23 10:18:33 +0000

    set root page to /dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true, // Uses a 308 permanent redirect status code
+      },
+    ];
+  },
 };
-
-export default nextConfig;
